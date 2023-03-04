83f008749023df7adfc71dfd35f5c8a538932cff
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-03-04 18:46:49 +0100

    init project

---
+++ src/main/java/hr/tvz/pious2023/Pious2023Application.java
@@ -0,0 +1,13 @@
+package hr.tvz.pious2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pious2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pious2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tvz/pious2023/Pious2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.pious2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pious2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
